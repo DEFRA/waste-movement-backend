d170e6427016ba19c86a9025d2344a3cd31bb447
Adam <adam@lumesoftware.com>
2025-07-24 14:02:35 +0100

    DWT-367 remove pops endpoint

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -2,7 +2,6 @@ import { health } from '../routes/health.js'
 import { createReceiptMovement } from '../routes/create-receipt-movement.js'
 import { updateReceiptMovement } from '../routes/update-receipt-movement.js'
 import { updateHazardousWaste } from '../routes/update-hazardous-waste.js'
-import { updateReceiptMovementPops } from '../routes/update-receipt-movement-pops.js'
 
 const router = {
   plugin: {
@@ -13,8 +12,7 @@ const router = {
         health,
         ...createReceiptMovement,
         updateReceiptMovement,
-        updateHazardousWaste,
-        updateReceiptMovementPops
+        updateHazardousWaste
       ]
 
       // Register routes directly
