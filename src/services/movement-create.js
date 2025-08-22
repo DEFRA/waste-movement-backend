8fdd5df0f423e5e52ed99f9bd558f107fe83fdf4
Michael Young <michael.young@esynergy.co.uk>
2025-08-22 13:50:51 +0100

    Add untested code

--- src/services/movement-create.js
+++ src/services/movement-create.js
@@ -3,5 +3,9 @@ export async function createWasteInput(db, wasteInput) {
   wasteInput.revision = 1
   const collection = db.collection('waste-inputs')
   const result = await collection.insertOne(wasteInput)
+
+  if (wasteInput.michaelTest === 'test') {
+    return 'Untested'
+  }
   return { _id: result.insertedId }
 }
