590c1951fb58972432593272e7fcd7c3cbce9e16
David Shepherd <david@x-designs.co.uk>
2025-11-11 16:17:28 +0000

    DWT-1063 Add exponential backoff delay to createWasteInput()

--- src/services/movement-create.test.js
+++ src/services/movement-create.test.js
@@ -1,4 +1,5 @@
 import { createWasteInput } from './movement-create.js'
+import * as exponentialBackoff from '../common/helpers/exponential-backoff-delay.js'
 
 describe('createWasteInput', () => {
   let mockDb
@@ -6,7 +7,6 @@ describe('createWasteInput', () => {
   let mockInsertOne
 
   beforeEach(() => {
-    // Setup mock collection and insertOne function
     mockInsertOne = jest.fn()
     mockCollection = {
       insertOne: mockInsertOne
@@ -17,7 +17,6 @@ describe('createWasteInput', () => {
   })
 
   it('should create a movement and return it with the inserted ID', async () => {
-    // Arrange
     const mockInsertedId = '123456789'
     const mockMovement = {
       wasteTrackingId: mockInsertedId
@@ -25,10 +24,8 @@ describe('createWasteInput', () => {
 
     mockInsertOne.mockResolvedValueOnce({ insertedId: mockInsertedId })
 
-    // Act
     const result = await createWasteInput(mockDb, mockMovement)
 
-    // Assert
     expect(mockDb.collection).toHaveBeenCalledWith('waste-inputs')
     expect(mockInsertOne).toHaveBeenCalledWith(mockMovement)
     expect(mockMovement.createdAt).toBeInstanceOf(Date)
@@ -40,20 +37,51 @@ describe('createWasteInput', () => {
   })
 
   it('should handle database errors', async () => {
-    // Arrange
     const mockMovement = {
       wasteTrackingId: '124453465'
     }
     const mockError = new Error('Database error')
     mockInsertOne.mockRejectedValueOnce(mockError)
 
-    // Act & Assert
-    await expect(createWasteInput(mockDb, mockMovement)).rejects.toThrow(
-      mockError
+    await expect(createWasteInput(mockDb, mockMovement, 6)).rejects.toThrow(
+      mockError.message
     )
+
     expect(mockDb.collection).toHaveBeenCalledWith('waste-inputs')
     expect(mockInsertOne).toHaveBeenCalledWith(mockMovement)
     expect(mockMovement.createdAt).toBeInstanceOf(Date)
     expect(mockMovement.lastUpdatedAt).toBeInstanceOf(Date)
   })
+
+  it('should handle exponential backoff twice', async () => {
+    const mockMovement = {
+      wasteTrackingId: '124453465'
+    }
+    const mockError = new Error('Database error')
+
+    mockDb = {
+      collection: jest
+        .fn()
+        .mockImplementationOnce(() => {
+          throw new Error(mockError)
+        })
+        .mockImplementationOnce(() => {
+          throw new Error(mockError)
+        })
+        .mockImplementation(() => ({
+          insertOne: () => ({ insertedId: 1 })
+        }))
+    }
+
+    const calculateExponentialBackoffDelaySpy = jest.spyOn(
+      exponentialBackoff,
+      'calculateExponentialBackoffDelay'
+    )
+
+    await createWasteInput(mockDb, mockMovement)
+
+    expect(calculateExponentialBackoffDelaySpy).toHaveBeenCalledWith(0)
+    expect(calculateExponentialBackoffDelaySpy).toHaveBeenCalledWith(1)
+    expect(calculateExponentialBackoffDelaySpy).toHaveBeenCalledTimes(2)
+  }, 16000)
 })
