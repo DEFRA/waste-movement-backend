50d13a374a1772c63f7eb17183c82724618a8cfb
David Shepherd <david@x-designs.co.uk>
2025-11-11 16:09:46 +0000

    DWT-1063 Add method to calculate an exponential backoff delay

---
+++ src/common/helpers/exponential-backoff-delay.js
@@ -0,0 +1,21 @@
+import {
+  INITIAL_DELAY_MS,
+  MAX_DELAY_MS
+} from '../constants/exponential-backoff-delay'
+
+/**
+ * Calculates an exponential backoff delay in milliseconds using the given retry depth
+ * and a start delay of 500ms, which doubles for each additional depth until a max delay
+ * of 8000ms
+ * @param {Number} depth - The current retry depth
+ * @returns {Object} { hasDelay, delay }
+ */
+export function calculateExponentialBackoffDelay(depth) {
+  const delay = INITIAL_DELAY_MS * 2 ** depth
+  const hasDelay = delay <= MAX_DELAY_MS
+
+  return {
+    hasDelay,
+    delay: hasDelay ? delay : undefined
+  }
+}
