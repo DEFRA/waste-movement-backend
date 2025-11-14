90136b35073a2ea00367992d60b5430a11661c29
David Shepherd <david@x-designs.co.uk>
2025-11-14 17:34:24 +0000

    DWT-1063 Update createReceiptMovement to use exponential-backoff lib rather than custom implementation

--- src/common/constants/exponential-backoff-delay.js
+++ src/common/constants/exponential-backoff-delay.js
@@ -1,2 +1,15 @@
+import { createLogger } from '../helpers/logging/logger.js'
+
 export const INITIAL_DELAY_MS = 500
 export const MAX_DELAY_MS = 8000
+
+const logger = createLogger()
+
+// See https://www.npmjs.com/package/exponential-backoff for all options
+export const BACKOFF_OPTIONS = {
+  numOfAttempts: 8,
+  retry: (error, attemptNumber) => {
+    logger.error(`Backoff attempt ${attemptNumber}: ${error.message}`)
+    return true
+  }
+}
