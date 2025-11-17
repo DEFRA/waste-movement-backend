56b93327d80e31e58a5fa190d98ef79a68166120
David Shepherd <david@x-designs.co.uk>
2025-11-17 10:51:11 +0000

    DWT-1063 Add BACKOFF_OPTIONS.retry() unit test

---
+++ src/common/constants/exponential-backoff.test.js
@@ -0,0 +1,17 @@
+import { BACKOFF_OPTIONS } from './exponential-backoff.js'
+import * as logger from '../helpers/logging/logger.js'
+
+describe('BACKOFF_OPTIONS tests', () => {
+  describe('#retry', () => {
+    it('should log an error message and return true', () => {
+      const errorLoggerSpy = jest.spyOn(logger.createLogger(), 'error')
+
+      const result = BACKOFF_OPTIONS.retry(new Error('Database Error'), 1)
+
+      expect(errorLoggerSpy).toHaveBeenCalledWith(
+        'Backoff attempt 1: Database Error'
+      )
+      expect(result).toBeTruthy()
+    })
+  })
+})
