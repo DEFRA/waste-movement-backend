bf1a2b0b0ac84280a624199913869e380b41617e
David Shepherd <david@x-designs.co.uk>
2025-11-27 17:16:11 +0000

    DWT-919 Add CDP audit logger and create auditLogger() wrapper

---
+++ src/common/helpers/logging/audit-logger.js
@@ -0,0 +1,53 @@
+import { audit } from '@defra/cdp-auditing'
+import { createLogger } from './logger.js'
+import { AUDIT_LOGGER_TYPE } from '../../constants/audit-logger.js'
+
+const logger = createLogger()
+
+/**
+ * Logs a message to the CDP audit endpoint
+ * @param params - The params to use to call the audit endpoint
+ * @param params.type - The audit log type, see AUDIT_LOGGER_TYPE
+ * @param params.correlationId - The audit log correlation id, for example request.getTraceId
+ * @param params.version - The version of the audit logger that is being used
+ * @param params.data - An object containing the audit log data
+ * @param params.fieldsToExcludeFromLoggedData - The fields that should be excluded from the data logged in an error message
+ * @param params.shouldThrowError - Determines if an error should be thrown
+ * @returns {Boolean} True if the audit endpoint has been called successfully
+ */
+export function auditLogger({
+  type,
+  correlationId,
+  version = 1,
+  data,
+  fieldsToExcludeFromLoggedData,
+  shouldThrowError = false
+}) {
+  const auditTypes = Object.values(AUDIT_LOGGER_TYPE)
+
+  try {
+    if (!auditTypes.includes(type)) {
+      throw new Error(`Audit type must be one of: ${auditTypes.join(', ')}`)
+    }
+
+    if (typeof data !== 'object') {
+      throw new Error('Audit data must be provided as an object')
+    }
+
+    audit({ type, correlationId, version, data })
+
+    return true
+  } catch (error) {
+    if (Array.isArray(fieldsToExcludeFromLoggedData)) {
+      fieldsToExcludeFromLoggedData.forEach((field) => delete data[field])
+    }
+
+    logger.error(data, `Failed to call audit endpoint: ${error.message}`)
+
+    if (shouldThrowError) {
+      throw new Error(`Failed to call audit endpoint: ${error.message}`)
+    }
+  }
+
+  return false
+}
