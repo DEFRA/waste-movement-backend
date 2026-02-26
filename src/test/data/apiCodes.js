fe4a4ab1664c70844634194537c60914f91309b0
David Shepherd <david@x-designs.co.uk>
2026-02-26 09:14:11 +0000

    DWT-1493 Skip the API Code secret validation when submittingOrganisation is present
    This is required because getting the submitting organisation will validate the API Code and whilst we transition away from the API Code secret unlikely that it will be kept up to date with new API Codes

--- src/test/data/apiCodes.js
+++ src/test/data/apiCodes.js
@@ -1,7 +1,9 @@
 export const apiCode1 = '25b14080-5e77-4f91-9957-2482a0cb8775'
 export const apiCode2 = 'bc05d1ce-5a80-4624-b2ae-e7227c8c6c41'
+export const apiCode3 = 'cb13cd1d-efa2-4172-9918-f4c47c53dd74'
 export const orgId1 = '57aed195-325e-45d5-b1fb-5f201e0324cf'
 export const orgId2 = '70d84972-2ad3-4ada-a867-ad261a7245e7'
+export const orgId3 = '7bbbe5a1a-82fa-48bf-bb8c-b516b8aa1ef4'
 
 export const orgApiCodes = [
   {
