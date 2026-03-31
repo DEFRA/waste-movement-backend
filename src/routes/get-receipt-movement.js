756242567cc2214c5bcbf085cc97ecaf0ae7baa1
David Shepherd <david@x-designs.co.uk>
2026-03-31 15:08:31 +0100

    DWT-1304 Update getReceiptMovement to use built in notFound function when no waste inputs are found

--- src/routes/get-receipt-movement.js
+++ src/routes/get-receipt-movement.js
@@ -2,6 +2,7 @@ import { HTTP_STATUS_CODES } from '../common/constants/http-status-codes.js'
 import { handleRouteError } from '../common/helpers/bulk-route-helpers.js'
 import { getWasteInputs } from '../services/movement-get.js'
 import { getReceiptMovementSchema } from '../schemas/get-receipt-movement.js'
+import { notFound } from '@hapi/boom'
 
 const getReceiptMovement = {
   method: 'GET',
@@ -25,13 +26,9 @@ const getReceiptMovement = {
       })
 
       if (wasteInputs.length === 0) {
-        return h
-          .response({
-            statusCode: HTTP_STATUS_CODES.NOT_FOUND,
-            error: 'Not Found',
-            message: `Waste inputs with values ${JSON.stringify({ wasteTrackingId, bulkId })} not found`
-          })
-          .code(HTTP_STATUS_CODES.NOT_FOUND)
+        return notFound(
+          `Waste inputs with values ${JSON.stringify({ wasteTrackingId, bulkId })} not found`
+        )
       }
 
       return h.response(wasteInputs).code(HTTP_STATUS_CODES.OK)
