59f44f49f9409ceb6a2ca80ab9e4ab8a46e3f2bf
David Shepherd <6839214+shepda@users.noreply.github.com>
2026-03-31 16:00:52 +0100

    DWT-1304 Add endpoint to get waste inputs for a wasteTrackingId or bulkId (#86)
    * DWT-1304 Add endpoint to get waste inputs for a wasteTrackingId or bulkId
    
    * DWT-1304 Update getReceiptMovement to use built in notFound function when no waste inputs are found

---
+++ src/routes/get-receipt-movement.js
@@ -0,0 +1,41 @@
+import { HTTP_STATUS_CODES } from '../common/constants/http-status-codes.js'
+import { handleRouteError } from '../common/helpers/bulk-route-helpers.js'
+import { getWasteInputs } from '../services/movement-get.js'
+import { getReceiptMovementSchema } from '../schemas/get-receipt-movement.js'
+import { notFound } from '@hapi/boom'
+
+const getReceiptMovement = {
+  method: 'GET',
+  path: '/qa-non-prod/movements',
+  options: {
+    tags: ['movements'],
+    description: 'Gets one or more receipt movements',
+    validate: {
+      query: getReceiptMovementSchema
+    }
+  },
+  handler: async (request, h) => {
+    try {
+      const { wasteTrackingId, bulkId, includeHistory } = request.query
+
+      const wasteInputs = await getWasteInputs({
+        db: request.db,
+        wasteTrackingId,
+        bulkId,
+        includeHistory
+      })
+
+      if (wasteInputs.length === 0) {
+        return notFound(
+          `Waste inputs with values ${JSON.stringify({ wasteTrackingId, bulkId })} not found`
+        )
+      }
+
+      return h.response(wasteInputs).code(HTTP_STATUS_CODES.OK)
+    } catch (error) {
+      return handleRouteError(h, error)
+    }
+  }
+}
+
+export { getReceiptMovement }
