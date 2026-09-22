17455eb7e95ded6ec99b42f3ae4889668aa555d0
Dmitry Demenchuk <dmitry@demench.uk>
2026-09-22 14:24:13 +0100

    feat: implement create-movement with a producer payload

---
+++ src/routes/beta-2/create-movement.js
@@ -0,0 +1,62 @@
+import { randomUUID } from 'node:crypto'
+import {
+  createMovementId,
+  createMovementRecord
+} from '../../services/movement.js'
+import { HTTP_STATUS, backoffOptions } from '@defra/waste-movement-utils'
+import { getTraceId } from '@defra/hapi-tracing'
+import { backOff } from 'exponential-backoff'
+import { createLogger } from '../../common/helpers/logging/logger.js'
+import { getOrgIdForApiCode } from '../../common/helpers/validate-api-code.js'
+import { config } from '../../config.js'
+import { jsonSchemaValidator } from '../../schemas/validate/hapi-validator.js'
+import { handleBetaRouteError } from '../../common/helpers/bulk-route-helpers.js'
+
+const apiVersion = 'beta-2'
+const logger = createLogger({ apiVersion })
+
+const createMovement = {
+  method: 'POST',
+  path: '/movements',
+  options: {
+    description: 'Create a new waste movement',
+    validate: {
+      payload: jsonSchemaValidator(
+        'beta-2/creation/create-movement.schema.json'
+      )
+    }
+  },
+  handler: async (request, h) => {
+    const { apiCode } = request.payload
+
+    try {
+      const traceId = getTraceId() || randomUUID()
+      const orgId = getOrgIdForApiCode(apiCode, config.get('orgApiCodes'))
+      const movementId = await createMovementId()
+
+      await backOff(
+        () => createMovementRecord(request.db, { movementId, orgId }),
+        backoffOptions(createLogger)
+      )
+      const responseBody = {
+        data: { movementId },
+        validation: { warnings: [] }
+      }
+
+      logger.info(
+        `Successfully created waste movement with id ${movementId}`,
+        responseBody
+      )
+
+      return h
+        .response(responseBody)
+        .code(HTTP_STATUS.CREATED)
+        .header('x-request-id', traceId)
+        .message('Successfully created a waste movement')
+    } catch (error) {
+      return handleBetaRouteError(error)
+    }
+  }
+}
+
+export { createMovement }
---
+++ src/routes/beta-2/create-movement.test.js
@@ -0,0 +1,237 @@
+import { HTTP_STATUS } from '@defra/waste-movement-utils'
+import * as movementCreate from '../../services/movement.js'
+import { config } from '../../config.js'
+import {
+  apiCode1,
+  apiCode3,
+  base64EncodedOrgApiCodes
+} from '../../test/data/apiCodes.js'
+import {
+  requestBasicAuthTest1,
+  userBasicAuthTest1
+} from '../../test/data/basic-auth.js'
+import { createServer } from '../../server.js'
+
+const backoffOptionsConfig = { numOfAttempts: 3, startingDelay: 1 }
+
+jest.mock('@defra/cdp-auditing', () => ({
+  audit: jest.fn().mockReturnValue(true)
+}))
+
+jest.mock('@defra/waste-movement-utils', () => {
+  const originalModule = jest.requireActual('@defra/waste-movement-utils')
+
+  return {
+    ...originalModule,
+    backoffOptions: () => backoffOptionsConfig
+  }
+})
+
+jest.mock('../../common/helpers/http-client.js', () => ({
+  httpClients: {
+    wasteTracking: {
+      get: jest
+        .fn()
+        .mockResolvedValue({ payload: { wasteTrackingId: '26S8EYDJ' } })
+    }
+  }
+}))
+
+describe('movement Route Tests version: beta-2', () => {
+  let server
+  const endpointVersion = 'beta-2'
+  const errorMessage = 'Database connection failed'
+  const traceId = 'created-trace-id-123'
+  const apiCode = apiCode1
+  const producer = {
+    wasteSource: 'Household',
+    councilMovement: true
+  }
+  const goodPayload = { apiCode, producer }
+
+  beforeAll(async () => {
+    config.set('orgApiCodes', base64EncodedOrgApiCodes)
+
+    process.env.ACCESS_CRED_TEST1 = userBasicAuthTest1
+
+    server = await createServer()
+  })
+
+  afterAll(async () => {
+    await server.stop()
+  })
+
+  it('creates a movement', async () => {
+    const createMovementRecordSpy = jest
+      .spyOn(movementCreate, 'createMovementRecord')
+      .mockResolvedValue(goodPayload)
+
+    const { statusCode, result, headers } = await server.inject({
+      method: 'POST',
+      url: `/${endpointVersion}/movements`,
+      payload: goodPayload,
+      headers: {
+        'x-cdp-request-id': traceId,
+        Authorization: `Basic ${requestBasicAuthTest1}`
+      }
+    })
+
+    expect(statusCode).toEqual(HTTP_STATUS.CREATED)
+    expect(result).toEqual({
+      data: { movementId: expect.any(String) },
+      validation: { warnings: [] }
+    })
+    expect(headers['x-request-id']).toEqual(traceId)
+    expect(createMovementRecordSpy).toHaveBeenCalledTimes(1)
+  })
+
+  it('handles error when creating a movement fails', async () => {
+    const createMovementRecordSpy = jest
+      .spyOn(movementCreate, 'createMovementRecord')
+      .mockRejectedValue(new Error(errorMessage))
+
+    const { statusCode, result, headers } = await server.inject({
+      method: 'POST',
+      url: `/${endpointVersion}/movements`,
+      payload: goodPayload,
+      headers: {
+        'x-cdp-request-id': traceId,
+        Authorization: `Basic ${requestBasicAuthTest1}`
+      }
+    })
+
+    expect(statusCode).toEqual(HTTP_STATUS.INTERNAL_SERVER_ERROR)
+    expect(result).toEqual({
+      instance: '/beta-2/movements',
+      title: 'Internal Server Error',
+      type: 'https://waste-tracking.service.gov.uk/problems/internal-server-error'
+    })
+    expect(headers['content-type']).toContain('application/problem+json')
+
+    expect(createMovementRecordSpy).toHaveBeenCalledTimes(
+      backoffOptionsConfig.numOfAttempts
+    )
+  })
+
+  it('returns an error when apiCode is missing and does not create a movement', async () => {
+    const invalidPayload = { producer }
+    const createMovementRecordSpy = jest.spyOn(
+      movementCreate,
+      'createMovementRecord'
+    )
+
+    const { statusCode, result } = await server.inject({
+      method: 'POST',
+      url: `/${endpointVersion}/movements`,
+      payload: invalidPayload,
+      headers: {
+        'x-cdp-request-id': traceId,
+        Authorization: `Basic ${requestBasicAuthTest1}`
+      }
+    })
+
+    expect(statusCode).toEqual(HTTP_STATUS.BAD_REQUEST)
+    expect(result).toEqual({
+      detail: '1 validation error occurred',
+      errors: [
+        {
+          errorType: 'NotProvided',
+          message: '"apiCode" is required',
+          pointer: '/apiCode'
+        }
+      ],
+      instance: '/beta-2/movements',
+      title: 'Bad Request',
+      type: 'https://waste-tracking.service.gov.uk/problems/bad-request'
+    })
+
+    expect(createMovementRecordSpy).toHaveBeenCalledTimes(0)
+  })
+
+  it('returns an error when producer is missing and does not create a movement', async () => {
+    const invalidPayload = { apiCode }
+    const createMovementRecordSpy = jest.spyOn(
+      movementCreate,
+      'createMovementRecord'
+    )
+
+    const { statusCode, result } = await server.inject({
+      method: 'POST',
+      url: `/${endpointVersion}/movements`,
+      payload: invalidPayload,
+      headers: {
+        'x-cdp-request-id': traceId,
+        Authorization: `Basic ${requestBasicAuthTest1}`
+      }
+    })
+
+    expect(statusCode).toEqual(HTTP_STATUS.BAD_REQUEST)
+    expect(result).toEqual({
+      detail: '1 validation error occurred',
+      errors: [
+        {
+          errorType: 'NotProvided',
+          message: '"producer" is required',
+          pointer: '/producer'
+        }
+      ],
+      instance: '/beta-2/movements',
+      title: 'Bad Request',
+      type: 'https://waste-tracking.service.gov.uk/problems/bad-request'
+    })
+
+    expect(createMovementRecordSpy).toHaveBeenCalledTimes(0)
+  })
+
+  it('returns an error when apiCode validation fails and does not create a movement', async () => {
+    const invalidPayload = { apiCode: apiCode3, producer }
+    const createMovementRecordSpy = jest.spyOn(
+      movementCreate,
+      'createMovementRecord'
+    )
+
+    const { statusCode, result } = await server.inject({
+      method: 'POST',
+      url: `/${endpointVersion}/movements`,
+      payload: invalidPayload,
+      headers: {
+        'x-cdp-request-id': traceId,
+        Authorization: `Basic ${requestBasicAuthTest1}`
+      }
+    })
+
+    expect(result).toEqual({
+      detail: 'the API Code supplied is invalid',
+      instance: '/beta-2/movements',
+      title: 'Bad Request',
+      type: 'https://waste-tracking.service.gov.uk/problems/bad-request'
+    })
+    expect(statusCode).toEqual(HTTP_STATUS.BAD_REQUEST)
+    expect(createMovementRecordSpy).toHaveBeenCalledTimes(0)
+  })
+
+  it('should return 401 when request is unauthenticated', async () => {
+    const createMovementRecordSpy = jest.spyOn(
+      movementCreate,
+      'createMovementRecord'
+    )
+
+    const { statusCode, result } = await server.inject({
+      method: 'POST',
+      url: `/${endpointVersion}/movements`,
+      payload: goodPayload,
+      headers: {
+        'x-cdp-request-id': traceId
+      }
+    })
+
+    expect(result).toEqual({
+      detail: 'Missing authentication',
+      instance: `/${endpointVersion}/movements`,
+      title: 'Unauthorized',
+      type: 'https://waste-tracking.service.gov.uk/problems/unauthorized'
+    })
+    expect(statusCode).toEqual(HTTP_STATUS.UNAUTHORIZED)
+    expect(createMovementRecordSpy).toHaveBeenCalledTimes(0)
+  })
+})
